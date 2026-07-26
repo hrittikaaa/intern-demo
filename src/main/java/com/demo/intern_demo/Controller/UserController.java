0f21ca8eb9ec9216d6272bf4f129629db271daa9
Hrittika Saha <68088694+hrittikaaa@users.noreply.github.com>
2026-07-26 16:53:02 +0600

    Initialize Hello World endpoint and User Entity.

---
+++ src/main/java/com/demo/intern_demo/Controller/UserController.java
@@ -0,0 +1,25 @@
+package com.demo.intern_demo.controller;
+
+import com.demo.intern_demo.model.User;
+import com.demo.intern_demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
